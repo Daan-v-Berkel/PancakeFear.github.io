b76f3d736616213c984d15ae7d4187d8cf1564b4
Daan van Berkel <daanvb1990@hotmail.com>
2021-11-10 23:38:47 +0100

    added github repo button

---
+++ .history/script_20211110221303.js
@@ -0,0 +1,233 @@
+// base variables
+const startNode = [3, 6]
+const endNode = [22, 50]
+let grid = null;
+
+function createGrid(){// creates the grid (on loading of the page) both visual(html) and backend
+  grid = new Grid(60, 30);
+  let gridDiv = document.getElementById('grid');
+  let columns = gridDiv.offsetWidth / 20;
+  let rows = gridDiv.offsetHeight / 20;
+  
+  for (var i = 0; i < rows; i++){
+    var currentRow = [];
+    for (var o = 0; o < columns; o++){
+      var gridNode = document.createElement("div");
+      let newId = i.toString() + ',' + o.toString();
+      gridNode.id = newId;
+      if (i === startNode[0] && o === startNode[1]){
+        gridNode.className = 'startnode';
+        var gn = new GridNode(newId, 'startnode')
+        grid.start = gn;
+      }
+      else if (i === endNode[0] && o === endNode[1]){
+        gridNode.className = 'endnode';
+        var gn = new GridNode(newId, 'endnode')
+        grid.end = gn;
+      }else{
+        gridNode.className = 'node';
+        var gn = new GridNode(newId, 'node')
+      }
+      //add mousehandlers to each node.
+      
+      gridNode.onmouseover = mouseEnterHandler;
+      gridNode.onmousedown = mouseDownHandler;
+      gridNode.oncontextmenu = rightMouseHandler;
+      currentRow.push(gn);
+      gridDiv.appendChild(gridNode);
+    }
+    //pushes each 'row' of nodes into the array, used to find nodes in the grid with id.
+    grid.nodes.push(currentRow);
+  }
+  return grid;
+}
+
+function mouseDownHandler(e){
+  if (grid.active){
+    return;
+  }
+  e.preventDefault();
+  grid.mousePressed = true;
+  var id = e.target.id;
+  grid.currentNode = grid.getNode(id);
+  grid.pressedNodeStatus = setMouseStatus(e);
+  if (e.which === 2){
+    if (!grid.pressedNodeStatus.includes('waypoint')){
+      var idToRemove = grid.addWaypoint(grid.currentNode);
+      if (idToRemove){
+        document.getElementById(idToRemove).className = 'node';
+      }
+      //set all divs to right class
+      for (var i in grid.waypoints){
+        var node = grid.waypoints[i];
+        var div = document.getElementById(node.id);
+        div.className = node.status;
+      }
+      e.target.className = grid.currentNode.status;
+      return;
+    }else{
+      grid.removeWaypoint(grid.currentNode);
+      for (var i in grid.waypoints){
+        var node = grid.waypoints[i];
+        var div = document.getElementById(node.id);
+        div.className = node.status;
+      }
+      grid.currentNode.status = 'node';
+      e.target.className = 'node';
+      return;
+    }
+  }
+  if (e.which === 3 && !(grid.currentNode.status.includes('start') || grid.currentNode.status.includes('end') || grid.currentNode.status.includes('waypoint'))){
+    if (grid.currentNode.status.includes('weighted')){
+      grid.pressedNodeStatus = 'node';
+      grid.currentNode.status = 'node';
+      e.target.className = grid.currentNode.status;
+      return;
+    }else{
+      grid.pressedNodeStatus = 'node weighted';
+      grid.currentNode.overwriteStatus('weighted');
+      e.target.className = grid.currentNode.status;
+      return;
+    }
+  }
+  if (grid.pressedNodeStatus === 'endnode' || grid.pressedNodeStatus === 'startnode' || grid.pressedNodeStatus.includes('waypoint')){
+    return;
+  }else if (!grid.pressedNodeStatus.includes('wall')){
+    grid.currentNode.status = 'node wall';
+    e.target.className = grid.currentNode.status;
+  }else if (grid.pressedNodeStatus.includes('wall')){
+    grid.currentNode.status = 'node';
+    e.target.className = grid.currentNode.status;
+  }
+}
+
+function rightMouseHandler(e){
+  if (grid.active){
+    return;
+  }
+  e.preventDefault();
+}
+
+function mouseUpHandler(e){
+  if (grid.active){
+    return;
+  }
+  addWeights();
+  handleWaypoints();
+  grid.mousePressed = false;
+  grid.mouseState = null;
+  grid.pressedNodeStatus = null;
+  grid.currentNode = null;
+  grid.previousNode = null;
+
+  var gridDiv = document.getElementById('grid');
+  startPosId = gridDiv.getElementsByClassName('startnode')[0].id;
+  endPosId = gridDiv.getElementsByClassName('endnode')[0].id;
+  grid.start = grid.getNode(startPosId);
+  grid.end = grid.getNode(endPosId);
+  console.log(grid.start);
+}
+
+function mouseEnterHandler(e){
+  if (grid.active){
+    return;
+  }
+  if (grid.mousePressed){
+    if (e.which === 2){
+      return;
+    }
+    let thisElement = e.target;
+    var id = e.target.id;
+    var prevElement = document.getElementById(grid.currentNode.id);
+    if (!(thisElement.className.includes('start') || thisElement.className.includes('end') || thisElement.className.includes('waypoint'))){
+      grid.previousNode = grid.currentNode;
+      grid.currentNode = grid.getNode(id);
+      switch (grid.pressedNodeStatus){
+        case "node wall":
+          grid.currentNode.status = 'node';
+          thisElement.className = grid.currentNode.status;
+          break;
+        case 'node weighted':
+          grid.currentNode.overwriteStatus('weighted');
+          thisElement.className = grid.currentNode.status;
+          break;
+        case 'node':
+          grid.currentNode.overwriteStatus('wall');
+          thisElement.className = grid.currentNode.status;
+          break;
+        default:
+          grid.currentNode.status = grid.pressedNodeStatus;
+          thisElement.className = grid.pressedNodeStatus;
+          grid.previousNode.status = 'node';
+          prevElement.className = grid.previousNode.status;
+          break;
+      }
+    }
+  }
+}
+
+function setMouseStatus(targetElement){
+  if (targetElement){
+    return targetElement.target.className;
+  }
+  return null;
+}
+
+function SelectSpeed(elem){
+  if (grid.active){
+    return;
+  }
+  var gridElement = document.getElementById('grid');
+  var nodeElements = gridElement.getElementsByClassName('node');
+  var btn = document.getElementById("dropdown-btn");
+  var speedString = elem.innerHTML;
+  var speed = null;
+
+  switch (speedString){
+    case "slow":
+      speed = 500;
+      break;
+    case "medium":
+      speed = 200;
+      break;
+    case "fast":
+      speed = 100;
+      break;
+    case "no delay":
+      speed = null;
+      break;
+    default:
+      speed = 100;
+      break;
+  }
+  grid.speed = speed;
+  btn.innerHTML = `Speed: ${speedString}`;
+
+  if (!grid.speed){
+    for (var node of nodeElements){
+      node.style.transition = 'none';
+    }
+  }else{
+    for (var node of nodeElements){
+      node.style.transition = '0.6s ease-out'
+    }
+  }
+}
+
+var popup = document.getElementById("popup");
+var btn = document.getElementById("popup-btn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  popup.style.display = "block";
+}
+
+span.onclick = function() {
+  popup.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
+}
